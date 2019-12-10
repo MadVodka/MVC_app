de9c0481705cfaf16403c2060cb186609e34cd74
Ivan Vatlin <ivanshblu@gmail.com>
2019-12-10 16:22:22 +0400

    Spring boot and soap integration

--- src/main/java/ivan/vatlin/configuration/SoapConfig.java
+++ src/main/java/ivan/vatlin/configuration/SoapConfig.java
@@ -1,5 +1,7 @@
 package ivan.vatlin.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -17,22 +19,29 @@ import org.springframework.xml.xsd.XsdSchema;
 @Configuration
 @ComponentScan("ivan.vatlin.soap.cars")
 public class SoapConfig extends WsConfigurerAdapter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SoapConfig.class);
+
+        private static final String NAMESPACE = "https://www.ivan.vatlin/cars";
 
     @Bean
     public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
+        LOGGER.info("messageDispatcherServlet initialized");
         return new ServletRegistrationBean(servlet, "/soap/*");
     }
 
-    @Bean
+    @Bean(name = "cars")
     public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema carsSchema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("CarDetailsPort");
         wsdl11Definition.setLocationUri("/soap/cars");
-        wsdl11Definition.setTargetNamespace("https://www.ivan.vatlin/cars");
+        wsdl11Definition.setTargetNamespace(NAMESPACE);
         wsdl11Definition.setSchema(carsSchema);
+
+        LOGGER.info("defaultWsdl11Definition initialized");
+
         return wsdl11Definition;
     }
 
--- src/main/java/ivan/vatlin/initializer/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package ivan.vatlin.initializer;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
