c0a97c96bd143660248f6d1a35eff0283952530c
Ivan Vatlin <ivanshblu@gmail.com>
2019-12-18 18:29:42 +0400

    spring-security-taglibs added

--- src/main/java/ivan/vatlin/controllers/OrderController.java
+++ src/main/java/ivan/vatlin/controllers/OrderController.java
@@ -9,7 +9,6 @@ import ivan.vatlin.services.CarService;
 import ivan.vatlin.services.OrderService;
 import ivan.vatlin.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
